0744f02eb77f08668ebb3ea85692ff5b4727b2a3
Marigowda, Santosh <santosharakere@gmail.com>
2018-12-16 08:36:30 +0000

    work in progress

--- exercises/api/server.js
+++ exercises/api/server.js
@@ -7,10 +7,20 @@ const path = require('path')
  * this function is blocking, fix that
  * @param {String} name full file name of asset in asset folder
  */
-const findAsset = (name) => {
-  const assetPath = path.join(__dirname, 'assets', name)
-  return fs.readFileSync(assetPath, {encoding: 'utf-8'}).toString()
+const findAsset = name => {
+  new Promise((resolve, reject) => {
+    const assetPath = path.join(__dirname, 'assets', name)
+    fs.readFile(assetPath, { encoding: 'utf-8' }, (error, data) => {
+      if(error) {
+        reject(error)
+      }
+      resolve(data.toString())
+    })
+    // return fs.readFile(assetPath, {encoding: 'utf-8'}).toString()  
+  })
 }
+
+
 
 const hostname = '127.0.0.1'
 const port = 3000
@@ -18,13 +28,13 @@ const port = 3000
 // log incoming request coming into the server. Helpful for debugging and tracking
 const logRequest = (method, route, status) => console.log(method, route, status)
 
-const server = http.createServer((req, res) => {
-  const method = req.method
-  const route = url.parse(req.url).pathname
+const server = http.createServer(async (req, res) => {
+  const method = req.method;
+  const route = url.parse(req.url).pathname;
   // this is sloppy, espcially with more assets, create a "router"
   if (route === '/') {
     res.writeHead(200, {'Content-Type': 'text/html'})
-    res.write(findAsset('index.html'))
+    res.write(await findAsset('index.html'))
     logRequest(method, route, 200)
     res.end()
   } else {
