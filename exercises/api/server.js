42990338f3f4cdf28572a0592ffda69c19adfce1
Marigowda, Santosh <santosharakere@gmail.com>
2018-12-16 12:35:50 +0000

    findAsset made async

--- exercises/api/server.js
+++ exercises/api/server.js
@@ -7,20 +7,19 @@ const path = require('path')
  * this function is blocking, fix that
  * @param {String} name full file name of asset in asset folder
  */
-const findAsset = name => {
-  new Promise((resolve, reject) => {
-    const assetPath = path.join(__dirname, 'assets', name)
-    fs.readFile(assetPath, { encoding: 'utf-8' }, (error, data) => {
+const findAsset = (name) => {
+  return new Promise((resolve, reject) => {
+    const assetPath = path.join(__dirname, 'assets', name);
+    fs.readFile(assetPath, {encoding: 'utf-8'}, (error, data) => {
       if(error) {
-        reject(error)
+        reject(error);
+      } else {
+        resolve(data);
       }
-      resolve(data.toString())
-    })
-    // return fs.readFile(assetPath, {encoding: 'utf-8'}).toString()  
+    });
   })
+  
 }
-
-
 
 const hostname = '127.0.0.1'
 const port = 3000
@@ -29,8 +28,8 @@ const port = 3000
 const logRequest = (method, route, status) => console.log(method, route, status)
 
 const server = http.createServer(async (req, res) => {
-  const method = req.method;
-  const route = url.parse(req.url).pathname;
+  const method = req.method
+  const route = url.parse(req.url).pathname
   // this is sloppy, espcially with more assets, create a "router"
   if (route === '/') {
     res.writeHead(200, {'Content-Type': 'text/html'})
